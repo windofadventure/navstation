34248673e493e235ff7b31d1cd3676d58f179893
mgrouch <mgrushinskiy@gmail.com>
2022-02-08 19:54:57 -0500

    solarthing

---
+++ install-scripts/8-apps/18-solarthing.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+curl https://raw.githubusercontent.com/wildmountainfarms/solarthing/master/other/linux/clone_install.sh | bash
+usermod -a -G solarthing,dialout,tty,video user
+/opt/solarthing/program/.bin/solarthing version
