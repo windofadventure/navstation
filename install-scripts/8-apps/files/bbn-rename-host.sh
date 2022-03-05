fad1257e5fbc575bf7f1f884f9e02ccb8dd9e158
mgrouch <mgrushinskiy@gmail.com>
2022-03-05 10:07:45 -0500

    upd

--- install-scripts/8-apps/files/bbn-rename-host.sh
+++ install-scripts/8-apps/files/bbn-rename-host.sh
@@ -15,6 +15,11 @@ fi
 NEW_HOSTNAME=$(echo $1 | tr '[:upper:]' '[:lower:]' | sed 's/[^0-9a-z-]*//g')
 OLD_HOSTNAME="${HOSTNAME:-lysmarine}"
 
+if [ -z "$NEW_HOSTNAME" ] ; then
+  echo "New host name can't be empty"
+  exit 3
+fi
+
 echo "renaming from $OLD_HOSTNAME to $NEW_HOSTNAME"
 
 sed -i "s/$OLD_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
