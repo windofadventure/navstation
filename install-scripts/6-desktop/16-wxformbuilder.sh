2d22545602902c2238a7062bf5301f2ccd79dc1c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-03-05 19:36:20 +0000

    Update 16-wxformbuilder.sh

--- install-scripts/6-desktop/16-wxformbuilder.sh
+++ install-scripts/6-desktop/16-wxformbuilder.sh
@@ -2,9 +2,10 @@
 
 apt-get -y install libwxgtk3.0-gtk3-dev libwxgtk-media3.0-gtk3-dev libboost-dev meson cmake make git
 
-git clone --recursive https://github.com/wxFormBuilder/wxFormBuilder
+git clone https://github.com/wxFormBuilder/wxFormBuilder
 cd wxFormBuilder
 git checkout 2d20e717ac918a5f8f4728146c29a3d83a6a3afd  # Nov 1, 2021
+git submodule update --recursive
 
 meson _build --prefix $PWD/_install --buildtype=release
 ninja -C _build install
