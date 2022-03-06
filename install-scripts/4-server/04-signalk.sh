c0a52c3becc998a9ff5d1162bce4096e9dc57bb9
mgrouch <mgrushinskiy@gmail.com>
2022-03-06 10:18:14 -0500

    upd

--- install-scripts/4-server/04-signalk.sh
+++ install-scripts/4-server/04-signalk.sh
@@ -14,6 +14,7 @@ install -m 644 -o signalk -g signalk $FILE_FOLDER/sk-to-nmea0183.json "/home/sig
 install -m 644 -o signalk -g signalk $FILE_FOLDER/signalk-path-filter.json "/home/signalk/.signalk/plugin-config-data/"
 install -m 644 -o signalk -g signalk $FILE_FOLDER/derived-data.json "/home/signalk/.signalk/plugin-config-data/"
 install -m 644 -o signalk -g signalk $FILE_FOLDER/charts.json "/home/signalk/.signalk/plugin-config-data/"
+install -m 644 -o signalk -g signalk $FILE_FOLDER/signalk-navtex-plugin.json "/home/signalk/.signalk/plugin-config-data/"
 
 install -m 644 -o signalk -g signalk $FILE_FOLDER/defaults.json "/home/signalk/.signalk/defaults.json"
 install -m 644 -o signalk -g signalk $FILE_FOLDER/package.json "/home/signalk/.signalk/package.json"
