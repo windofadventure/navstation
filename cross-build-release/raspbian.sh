e6cf57e8abdb7ab97494b0e7ba51e7687da5b191
mgrouch <mgrushinskiy@gmail.com>
2022-04-23 09:39:32 -0400

    fix

--- cross-build-release/raspbian.sh
+++ cross-build-release/raspbian.sh
@@ -7,7 +7,7 @@
 
   thisArch="raspios"
   cpuArch="armhf"
-  zipName="raspios_oldstable_lite_armhf/images/raspios_oldstable_lite_armhf-2022-04-07/2022-04-07-raspios-buster-armhf-lite.zip"
+  zipName="raspios_oldstable_lite_armhf/images/raspios_oldstable_lite_armhf-2022-04-07/2022-04-04-raspios-buster-armhf-lite.zip"
   if [ "arm64" == "$MY_CPU_ARCH" ]; then
     cpuArch="arm64"
     zipName="raspios_lite_arm64/images/raspios_lite_arm64-2021-05-28/2021-05-07-raspios-buster-arm64-lite.zip"
