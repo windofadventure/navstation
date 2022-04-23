3964de17caa53f9a6bdf41e78e86f6c413127118
mgrouch <mgrushinskiy@gmail.com>
2022-04-23 08:03:33 -0400

    for OpenCPN 5.6.2

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -39,8 +39,8 @@ fi
 mkdir tmp-o-bundle-$LMARCH || exit 2
 cd tmp-o-bundle-$LMARCH
 
-wget -O opencpn-plugins-bundle-$LMARCH.tar.gz https://github.com/bareboat-necessities/opencpn-plugins-bundle/blob/main/bundles/opencpn-plugins-bundle-$LMARCH.tar.gz?raw=true
-gzip -cd opencpn-plugins-bundle-$LMARCH.tar.gz | tar xvf -
+wget -O opencpn-plugins-bundle-o_5_6_x-$LMARCH.tar.gz https://github.com/bareboat-necessities/opencpn-plugins-bundle/blob/main/bundles/opencpn-plugins-bundle-o_5_6_x-$LMARCH.tar.gz?raw=true
+gzip -cd opencpn-plugins-bundle-o_5_6_x-$LMARCH.tar.gz | tar xvf -
 
 cp -r -p lib/* /usr/lib/
 cp -r -p bin/* /usr/bin/
