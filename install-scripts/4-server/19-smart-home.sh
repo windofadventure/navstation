c44c610c5057c17703448e1bd2ba58989d401437
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-04-28 02:03:58 +0100

    Update 19-smart-home.sh

--- install-scripts/4-server/19-smart-home.sh
+++ install-scripts/4-server/19-smart-home.sh
@@ -37,7 +37,7 @@ cat << EOF
   python3.9 -m venv .
   source bin/activate
   python3.9 -m pip install wheel
-  pip3 install homeassistant
+  pip3.9 install homeassistant
   mkdir -p /home/homeassistant/.homeassistant
   rm -rf /home/homeassistant/.cache
 EOF
@@ -73,7 +73,7 @@ cat << EOF
   python3.9 -m venv .
   source bin/activate
   python3.9 -m pip install wheel
-  pip3 install esphome tornado esptool
+  pip3.9 install esphome tornado esptool
   rm -rf /home/homeassistant/.cache
 EOF
 } | sudo -u homeassistant -H -s
