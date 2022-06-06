9985a63a5a8f8edf80c7b23038d0044bf9f8777c
mgrouch <mgrushinskiy@gmail.com>
2022-06-06 14:51:24 -0400

    for bullseye (trying to make budgie-desktop to start)

--- install-scripts/6-desktop/03-budgie-desktop.sh
+++ install-scripts/6-desktop/03-budgie-desktop.sh
@@ -14,11 +14,7 @@ install -o 1000 -g 1000 -m 644 -v $FILE_FOLDER/.xinitrc    "/home/user/"
   echo 'budgie-desktop --debug --whale &'
 }  >>/home/user/.config/openbox/autostart
 
-if [ $LMARCH == 'arm64' ]; then
-  echo 'chromium --headless &' >>/home/user/.config/openbox/autostart
-else
-  echo 'chromium-browser --headless &' >>/home/user/.config/openbox/autostart
-fi
+echo '(chromium-browser --headless || true) &' >>/home/user/.config/openbox/autostart
 
 apt-get clean
 
