a5e0135ece6a8eaad8a7a930a079e7358095e6fe
mgrouch <mgrushinskiy@gmail.com>
2022-08-05 11:06:41 -0400

    started merging

--- install-scripts/8-apps/09-more.sh
+++ install-scripts/8-apps/09-more.sh
@@ -5,7 +5,7 @@ npm cache clean --force
 
 apt-get -q -y install i2c-tools python3-smbus dos2unix traceroute telnet whois socat gdal-bin openvpn seahorse inxi \
   dconf-editor gedit gnome-calculator gnome-weather gnome-chess openpref \
-  python3-bme680 python3-gpiozero python3-w1thermsensor  \
+  python3-gpiozero  \
   sysstat jq xmlstarlet uhubctl iotop rsync timeshift restic at snapd \
   rpi-imager piclone fontconfig nautic gnome-disk-utility catfish xfce4-screenshooter hardinfo baobab # foxtrotgps
 
