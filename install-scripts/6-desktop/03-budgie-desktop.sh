4b15c1144a9c9f9df97670caa8f7abdf03804aac
mgrouch <mgrushinskiy@gmail.com>
2022-08-05 15:36:13 -0400

    fixing

--- install-scripts/6-desktop/03-budgie-desktop.sh
+++ install-scripts/6-desktop/03-budgie-desktop.sh
@@ -3,7 +3,7 @@
 apt-get install -y -q libatk-adaptor libgtk-3-0 libatk1.0-0 libcairo2 libfontconfig1 libfreetype6 \
   libgdk-pixbuf2.0-0 libglib2.0-0  \
   libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 librsvg2-common libx11-6 menulibre \
-  gvfs-fuse gvfs-backends gnome-bluetooth ibus openbox
+  gvfs-fuse gvfs-backends gnome-bluetooth ibus
 
 install -o 1000 -g 1000 -d /home/user/.config/openbox
 
