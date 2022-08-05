a45d2cb2d02b42dec424dc6645db3624766a4fab
mgrouch <mgrushinskiy@gmail.com>
2022-08-04 20:36:25 -0400

    bullseye

--- install-scripts/6-desktop/03-budgie-desktop.sh
+++ install-scripts/6-desktop/03-budgie-desktop.sh
@@ -3,16 +3,16 @@
 apt-get install -y -q libatk-adaptor libgtk-3-0 libatk1.0-0 libcairo2 libfontconfig1 libfreetype6 \
   libgdk-pixbuf2.0-0 libglib2.0-0  \
   libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 librsvg2-common libx11-6 menulibre \
-  gvfs-fuse gvfs-backends gnome-bluetooth ibus openbox
+  gvfs-fuse gvfs-backends gnome-bluetooth ibus #openbox
 
 install -o 1000 -g 1000 -d /home/user/.config/openbox
 
 ## Start budgie-desktop on openbox boot.
-#install -o 1000 -g 1000 -m 644 -v $FILE_FOLDER/.xinitrc    "/home/user/"
-{
-  echo 'export XDG_CURRENT_DESKTOP=Budgie:GNOME'
-  echo 'budgie-desktop &'
-}  >>/home/user/.config/openbox/autostart
+install -o 1000 -g 1000 -m 644 -v $FILE_FOLDER/.xinitrc    "/home/user/"
+#{
+#  echo 'export XDG_CURRENT_DESKTOP=Budgie:GNOME'
+#  echo 'budgie-desktop &'
+#}  >>/home/user/.config/openbox/autostart
 
 #echo '(chromium-browser --headless || true) &' >>/home/user/.config/openbox/autostart
 
