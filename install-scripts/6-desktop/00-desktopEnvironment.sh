a58301a13cf256622c6d8537b9d3f1081aed35e8
mgrouch <mgrushinskiy@gmail.com>
2022-08-05 11:46:42 -0400

    started merging

--- install-scripts/6-desktop/00-desktopEnvironment.sh
+++ install-scripts/6-desktop/00-desktopEnvironment.sh
@@ -20,8 +20,13 @@ apt-get -q -y install xserver-xorg-input-libinput xinput libinput-tools xinput-c
  gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-alsa \
  gstreamer1.0-libav alsa-utils libavahi-compat-libdnssd-dev git \
  xbacklight lxappearance gmrun xsettingsd xserver-xorg \
- xinit cpanminus perl-base \
- dialog lxterminal network-manager-gnome system-config-printer
+ xinit cpanminus perl-base wmctrl \
+ dialog lxterminal network-manager-gnome system-config-printer \
+ lxterminal gpsbabel file-roller lxtask thunar git adapta-gtk-theme \
+ libqt5quickwidgets5 libqt5widgets5 libqt5gui5 libqt5webkit5 \
+ libqt5sql5 libqt5printsupport5 libqt5network5 libqt5serialport5 \
+ libqt5svg5 libqt5opengl5 libqt5test5 libqt5xml5 libqt5qml5 qml-module-qtquick-controls libsndfile1
+
 
 install -o 1000 -g 1000 -d /home/user/.local
 install -o 1000 -g 1000 -d /home/user/.local/share
