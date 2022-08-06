9208f6f3902e781e32601f90588a9b7d634a2bd0
mgrouch <mgrushinskiy@gmail.com>
2022-08-06 13:19:44 -0400

    fixing

--- install-scripts/2-baseSystem/01-boot.sh
+++ install-scripts/2-baseSystem/01-boot.sh
@@ -54,13 +54,13 @@ if [[ $LMOS == 'Raspbian' ]]; then
 	systemctl disable triggerhappy.service
 fi
 
-#
-#install -v -d "/etc/systemd/system/console-setup.service.d"
-#bash -c 'cat << EOF > /etc/systemd/system/console-setup.service.d/override.conf
+
+install -v -d "/etc/systemd/system/console-setup.service.d"
+bash -c 'cat << EOF > /etc/systemd/system/console-setup.service.d/override.conf
 #[Unit]
 #After=systemd-tmpfiles-setup.service
 #EOF'
-#
+
 #
 #install -v -d "/etc/systemd/system/keyboard-setup.service.d"
 #bash -c 'cat << EOF > /etc/systemd/system/keyboard-setup.service.d/override.conf
