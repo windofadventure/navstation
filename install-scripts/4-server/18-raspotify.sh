f412f13c2167ffb253b0c3cfda46e26a8b8a045d
mgrouch <mgrushinskiy@gmail.com>
2022-08-07 19:01:01 -0400

    fix

--- install-scripts/4-server/18-raspotify.sh
+++ install-scripts/4-server/18-raspotify.sh
@@ -2,17 +2,22 @@
 
 if [ $LMARCH == 'armhf' ]; then
   #apt-get -y -q install raspotify=0.31.8~librespot.v0.3.1-54-gf4be9bb
-  wget https://github.com/dtcooper/raspotify/blob/2d6ceca0921a632b73e45cb8dbed6b8a57b3b608/pool/main/r/raspotify/raspotify_0.31.8~librespot.v0.3.1-54-gf4be9bb_armhf.deb?raw=true -O rasp.deb
-  dpkg -i rasp.deb && rm -f rasp.deb
-  if ! grep -q raspotify /etc/group; then
-  	groupadd raspotify
-  fi
-  usermod -a -G raspotify user
-  systemctl disable raspotify
-  install -d -m 755 -o 1000 -g 1000 "/home/user/.config/systemd/"
-  install -d -m 755 -o 1000 -g 1000 "/home/user/.config/systemd/user/"
-  install -v -m 644 -o 1000 -g 1000 $FILE_FOLDER/raspotify.service "/home/user/.config/systemd/user/raspotify.service"
+  wget https://github.com/dtcooper/raspotify/releases/download/0.42.1/raspotify_0.42.1.librespot.v0.4.2-6537c44_armhf.deb -O rasp.deb
 fi
+if [ $LMARCH == 'arm64' ]; then
+  #apt-get -y -q install raspotify=0.31.8~librespot.v0.3.1-54-gf4be9bb
+  wget https://github.com/dtcooper/raspotify/releases/download/0.42.1/raspotify_0.42.1.librespot.v0.4.2-6537c44_arm64.deb -O rasp.deb
+fi
+
+dpkg -i rasp.deb && rm -f rasp.deb
+if ! grep -q raspotify /etc/group; then
+  groupadd raspotify
+fi
+usermod -a -G raspotify user
+systemctl disable raspotify
+install -d -m 755 -o 1000 -g 1000 "/home/user/.config/systemd/"
+install -d -m 755 -o 1000 -g 1000 "/home/user/.config/systemd/user/"
+install -v -m 644 -o 1000 -g 1000 $FILE_FOLDER/raspotify.service "/home/user/.config/systemd/user/raspotify.service"
 
 apt-get clean
 
