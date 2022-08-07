2667e8729c98b736ae2e74cd5f8eea0e0885ed21
mgrouch <mgrushinskiy@gmail.com>
2022-08-07 00:58:22 -0400

    fix

--- install-scripts/4-server/19-smart-home.sh
+++ install-scripts/4-server/19-smart-home.sh
@@ -27,7 +27,7 @@ cat << EOF
   python3.9 -m venv .
   source bin/activate
   python3.9 -m pip install wheel
-  pip3.9 install "homeassistant==2022.5.5"
+  pip3.9 install "homeassistant"
   mkdir -p /home/homeassistant/.homeassistant
   rm -rf /home/homeassistant/.cache
 EOF
