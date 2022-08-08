00b6ebac6abe0e0e3e3012ce6f9a86616f9d6441
mgrouch <mgrushinskiy@gmail.com>
2022-08-08 14:40:03 -0400

    upd

--- install-scripts/8-apps/11-instant-messaging.sh
+++ install-scripts/8-apps/11-instant-messaging.sh
@@ -10,6 +10,10 @@ if [ $LMARCH == 'armhf' ]; then
   arch=armv7l
 elif [ $LMARCH == 'arm64' ]; then
   arch=arm64
+  wget http://ftp.us.debian.org/debian/pool/main/libi/libindicator/libindicator3-7_0.5.0-4_arm64.deb
+  wget http://ftp.mx.debian.org/debian/pool/main/liba/libappindicator/libappindicator3-1_0.4.92-7_arm64.deb
+  dpkg -i libindicator3-7_0.5.0-4_arm64.deb libappindicator3-1_0.4.92-7_arm64.deb
+  rm libindicator3-7_0.5.0-4_arm64.deb libappindicator3-1_0.4.92-7_arm64.deb
 elif [ $LMARCH == 'amd64' ]; then
   arch=x64
 else
