4631705a2fb54e567684435757a54cd8cec3f059
mgrouch <mgrushinskiy@gmail.com>
2022-08-08 09:48:07 -0400

    fix

--- install-scripts/4-server/15-motioneye.sh
+++ install-scripts/4-server/15-motioneye.sh
@@ -18,6 +18,9 @@ mkdir -p /var/lib/motioneye
 
 install -m 755 -d -o motion -g motion "/var/log/motion"
 
+# resolve conflict with pypilot_web
+sed -i "s/8080/8480/g" /etc/motion/motion.conf
+
 #cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
 #systemctl daemon-reload
 systemctl enable motioneye
