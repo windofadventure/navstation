c9376147b9835a161905a4ba43f19c8f957b1bb0
mgrouch <mgrushinskiy@gmail.com>
2022-08-08 14:42:23 -0400

    upd

--- install-scripts/8-apps/11-instant-messaging.sh
+++ install-scripts/8-apps/11-instant-messaging.sh
@@ -10,18 +10,20 @@ if [ $LMARCH == 'armhf' ]; then
   arch=armv7l
 elif [ $LMARCH == 'arm64' ]; then
   arch=arm64
-  wget http://ftp.us.debian.org/debian/pool/main/libi/libindicator/libindicator3-7_0.5.0-4_arm64.deb
-  wget http://ftp.mx.debian.org/debian/pool/main/liba/libappindicator/libappindicator3-1_0.4.92-7_arm64.deb
-  dpkg -i libindicator3-7_0.5.0-4_arm64.deb libappindicator3-1_0.4.92-7_arm64.deb
-  rm libindicator3-7_0.5.0-4_arm64.deb libappindicator3-1_0.4.92-7_arm64.deb
 elif [ $LMARCH == 'amd64' ]; then
   arch=x64
 else
   arch=$LMARCH
 fi
 
+wget http://ftp.us.debian.org/debian/pool/main/libi/libindicator/libindicator3-7_0.5.0-4_${arch}.deb
+wget http://ftp.mx.debian.org/debian/pool/main/liba/libappindicator/libappindicator3-1_0.4.92-7_${arch}.deb
+dpkg -i libindicator3-7_0.5.0-4_${arch}.deb libappindicator3-1_0.4.92-7_${arch}.deb
+rm libindicator3-7_0.5.0-4_${arch}.deb libappindicator3-1_0.4.92-7_${arch}.deb
+
 wget https://github.com/mquevill/caprine/releases/download/v2.54.1-ARM/caprine_2.54.1_${arch}.deb
 
 dpkg -i caprine_2.54.1_${arch}.deb
 
 rm caprine_2.54.1_${arch}.deb
+
