e801390eb699167b2ddf28f4734d7a02540d2b37
mgrouch <mgrushinskiy@gmail.com>
2022-08-08 13:24:34 -0400

    upd

--- install-scripts/8-apps/14-ads-b.sh
+++ install-scripts/8-apps/14-ads-b.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -e
 
-if [ $LMARCH != 'arm64' ]; then
+apt-get -y -q install dump1090-fa piaware
 
-  apt-get -y -q install dump1090-fa piaware
+sed -i 's/= 80/= 8186/' /etc/lighttpd/lighttpd.conf
 
-  sed -i 's/= 80/= 8186/' /etc/lighttpd/lighttpd.conf
-
-  systemctl disable lighttpd
-  systemctl disable dump1090-fa
-  systemctl disable piaware
-fi
+systemctl disable lighttpd
+systemctl disable dump1090-fa
+systemctl disable piaware
