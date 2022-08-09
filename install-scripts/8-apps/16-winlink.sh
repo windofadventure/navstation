436c80ae15c473e31281d67a618c283733868494
mgrouch <mgrushinskiy@gmail.com>
2022-08-08 23:12:06 -0400

    upd

--- install-scripts/8-apps/16-winlink.sh
+++ install-scripts/8-apps/16-winlink.sh
@@ -5,15 +5,18 @@
 if [ $LMARCH == 'armhf' ]; then
   wget https://github.com/la5nta/pat/releases/download/v0.12.1/pat_0.12.1_linux_armhf.deb
   dpkg -i pat_0.12.1_linux_armhf.deb && rm pat_0.12.1_linux_armhf.deb
-
-  # See: https://www.cantab.net/users/john.wiseman/Documents/ARDOPC.html
-  wget -O ardopc http://www.cantab.net/users/john.wiseman/Downloads/Beta/piardopc
-  install ardopc /usr/local/bin && rm ardopc
-  if [ "$(grep 'pcm\.ARDOP' /home/user/.asoundrc |wc -l)" -lt "1" ]; then
-    echo 'pcm.ARDOP {type rate slave {pcm "hw:CARD=CODEC,DEV=0" rate 48000}}' >> /home/user/.asoundrc
-  fi
-  /home/user/add-ons/winlink-pat-install.sh
+else
+  wget https://dl.cloudsmith.io/public/bbn-projects/bbn-deb-repo/deb/debian/pool/bullseye/main/p/pa/pat_0.12.1-1/pat_0.12.1-1_arm64.deb
+  dpkg -i pat_0.12.1-1_arm64.deb && rm pat_0.12.1-1_arm64.deb
 fi
+# See: https://www.cantab.net/users/john.wiseman/Documents/ARDOPC.html
+wget -O ardopc http://www.cantab.net/users/john.wiseman/Downloads/Beta/piardopc
+install ardopc /usr/local/bin && rm ardopc
+if [ "$(grep 'pcm\.ARDOP' /home/user/.asoundrc |wc -l)" -lt "1" ]; then
+  echo 'pcm.ARDOP {type rate slave {pcm "hw:CARD=CODEC,DEV=0" rate 48000}}' >> /home/user/.asoundrc
+fi
+/home/user/add-ons/winlink-pat-install.sh
+
 
 # This is something else: https://bitbucket.org/VK2ETA/
 my_dir="$(pwd)"
