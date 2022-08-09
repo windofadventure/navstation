f6a6f44850bfae4ce5abdccbebdda1a81716c872
mgrouch <mgrushinskiy@gmail.com>
2022-08-08 20:10:26 -0400

    upd

--- cross-build-release/raspbian.sh
+++ cross-build-release/raspbian.sh
@@ -73,7 +73,7 @@ EOF
   ls -l ./work/$thisArch/$imageName
 
   # Renaming the OS and moving it to the release folder.
-  cp -v ./work/$thisArch/$imageName ./release/$thisArch/bbn-os-alpha_${LYSMARINE_VER}-${thisArch}-${cpuArch}.img
+  cp -v ./work/$thisArch/$imageName ./release/$thisArch/bbn-os-bullseye-alpha_${LYSMARINE_VER}-${thisArch}-${cpuArch}.img
 
   exit 0
 }
