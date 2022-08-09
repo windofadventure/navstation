0d964a3f4526f1b69b38e9a8418ef542d8eb61b2
mgrouch <mgrushinskiy@gmail.com>
2022-08-08 20:40:25 -0400

    upd

--- install-scripts/4-server/14-grafana.sh
+++ install-scripts/4-server/14-grafana.sh
@@ -7,6 +7,7 @@ apt-get -y -q install grafana
 systemctl disable grafana-server
 
 sed -i 's/3000/3080/g' /etc/grafana/grafana.ini
+sed -i 's/;http_port/http_port/g' /etc/grafana/grafana.ini
 
 apt-get clean
 
