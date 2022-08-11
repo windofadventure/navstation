c1e7986b4394b0f488e4b47e9e363ff11e557808
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-08-11 13:25:57 +0000

    Update 04-xorgAutostart.sh

--- install-scripts/6-desktop/04-xorgAutostart.sh
+++ install-scripts/6-desktop/04-xorgAutostart.sh
@@ -4,3 +4,7 @@ install -v -m644 $FILE_FOLDER/startx.service "/etc/systemd/system/startx.service
 
 systemctl enable startx.service
 systemctl set-default graphical.target
+
+
+ln -s /usr/lib/aarch64-linux-gnu/dri /usr/lib/dri
+
