f18a308863c55b8dbaaba02cff0d4f7acf6e9c9a
mgrouch <mgrushinskiy@gmail.com>
2022-08-21 23:16:42 -0400

    fixes

---
+++ install-scripts/4-server/21-victron.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+pushd /usr/share/
+  git clone https://github.com/victronenergy/venus-html5-app && cd venus-html5-app/
+  pnpm install
+  pnpm caniuse-lite
+popd
+
+# to start:
+#
+# cd /usr/share/venus-html5-app
+# npm run start
+#
+# And then open the app in the browser at http://localhost:8000
+#
+# This will start the webpack dev server, which will recompile the app on code changes and hot reload the UI.
+#
+# You can change the host and port (although the default 9001 is usually correct) query parameters to point to your Venus device:
+#
+# http://localhost:8000?host=<VENUS_DEVICE_IP>&port=9001
+#
+# This way you can run the local app against venus device data if the venus device is on the same network as your computer.
+
