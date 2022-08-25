0d9a4e1effb05d14cccaf07c9635f21fc0b2c2f4
mgrouch <mgrushinskiy@gmail.com>
2022-08-25 10:20:19 -0400

    upd

--- install-scripts/4-server/21-victron.sh
+++ install-scripts/4-server/21-victron.sh
@@ -2,25 +2,32 @@
 
 pushd /usr/share/
   git clone https://github.com/victronenergy/venus-html5-app && cd venus-html5-app/
-  pnpm install
-  pnpm install caniuse-lite
+  #pnpm install
+  pnpm install caniuse-lite date-fns
   pnpm run build
+  mkdir venus-app
+  cp -r dist venus-app/app
 popd
+
+npm install -g serve
+
 
 # to start:
 #
-# cd /usr/share/venus-html5-app
-# pnpm run start
+# cd /usr/share/venus-app
+# serve -l 8000
 #
-# And then open the app in the browser at http://localhost:8000
+# And then open the app in the browser at http://localhost:8000/app
 #
 # This will start the webpack dev server, which will recompile the app on code changes and hot reload the UI.
 #
-# You can change the host and port (although the default 9001 is usually correct) query parameters to point to your Venus device:
+# You can change the host and port (although the default 9001 is usually correct) query parameters to point
+# to your Venus device:
 #
-# http://localhost:8000?host=<VENUS_DEVICE_IP>&port=9001
+# http://localhost:8000/app?host=<VENUS_DEVICE_IP>&port=9001
 #
-# This way you can run the local app against venus device data if the venus device is on the same network as your computer.
+# This way you can run the local app against venus device data if the venus device
+# is on the same network as your computer.
 
 install -v -m 0644 $FILE_FOLDER/victron.service "/etc/systemd/system/"
 
@@ -32,8 +39,8 @@ Type=Application
 Name=Victron
 GenericName=Victron
 Comment=Victron
-Exec=gnome-www-browser http://localhost:8000
-#Exec=gnome-www-browser http://localhost:8000?host=<VENUS_DEVICE_IP>&port=9001
+Exec=gnome-www-browser http://localhost:8000/app
+#Exec=gnome-www-browser http://localhost:8000/app?host=<VENUS_DEVICE_IP>&port=9001
 # Read: https://github.com/victronenergy/venus-html5-app
 Terminal=false
 Icon=battery-symbolic
