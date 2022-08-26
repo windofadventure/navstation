8f8ea013c1be39b4bfb10de9cbf77303722a0b38
mgrouch <mgrushinskiy@gmail.com>
2022-08-26 19:12:44 -0400

    fix

--- install-scripts/4-server/02-pypilot.sh
+++ install-scripts/4-server/02-pypilot.sh
@@ -33,7 +33,7 @@ install -v -m 0644 $FILE_FOLDER/60-watchdog.rules "/etc/udev/rules.d/60-watchdog
 export MAKEFLAGS='-j 4'
 
 if [ $LMARCH == 'arm64' ]; then
-  pip3 install pywavefront pyglet gps gevent-websocket websocket-client importlib_metadata "python-socketio<5" "flask-socketio<5"
+  pip3 install pywavefront pyglet gps gevent-websocket websocket-client importlib_metadata python-socketio flask-socketio
 else
   apt-get install -y -q python3-flask-socketio
   pip3 install pywavefront pyglet gps gevent-websocket importlib_metadata "python-socketio<5"
