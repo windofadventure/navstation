287190f4d68c334e20a8dcee62d9e5b695c627f9
mgrouch <mgrushinskiy@gmail.com>
2022-08-29 08:27:12 -0400

    upd

--- install-scripts/8-apps/17-arduino.sh
+++ install-scripts/8-apps/17-arduino.sh
@@ -2,14 +2,14 @@
 
 pushd /opt
   ARD_URL=https://downloads.arduino.cc
-  ARD_FILE=arduino-1.8.16-linuxarm.tar.xz
+  ARD_FILE=arduino-1.8.19-linuxarm.tar.xz
   if [ $LMARCH == 'arm64' ]; then
-    ARD_FILE=arduino-1.8.16-linuxaarch64.tar.xz
+    ARD_FILE=arduino-1.8.19-linuxaarch64.tar.xz
   fi
   wget $ARD_URL/$ARD_FILE
   xzcat $ARD_FILE | tar xvf -
   rm $ARD_FILE
-  pushd arduino-1.8.16
+  pushd arduino-1.8.19
     install -d /root/.config
     ./install.sh
     ./arduino-linux-setup.sh root
