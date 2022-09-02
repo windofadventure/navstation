5682d38423968aafa6cc8b718ea6e2c9515496b2
mgrouch <mgrushinskiy@gmail.com>
2022-09-02 17:58:05 -0400

    upd

--- install-scripts/a-cleanup/00-cleanup.sh
+++ install-scripts/a-cleanup/00-cleanup.sh
@@ -53,6 +53,8 @@ done
 mv /etc/apt/sources.list.d/bbn-rce.list-orig /etc/apt/sources.list.d/bbn-rce.list
 mv /etc/apt/sources.list.d/bbn-kplex.list-orig /etc/apt/sources.list.d/bbn-kplex.list
 
+rm /etc/apt/sources.list.d/*.list-orig
+
 install -v -m 0644 $FILE_FOLDER/rsyslog "/etc/logrotate.d/rsyslog"
 
 # Fill free space with zeros
