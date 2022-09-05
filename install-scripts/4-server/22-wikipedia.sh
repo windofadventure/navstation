ee98e1769bae89445b509d3283d6cba4cf2308f0
mgrouch <mgrushinskiy@gmail.com>
2022-09-04 21:47:39 -0400

    kiwix

--- install-scripts/4-server/22-wikipedia.sh
+++ install-scripts/4-server/22-wikipedia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt-get install -y liblzma libicu libzstd libxapian libcurl4-gnutls
+apt-get install -y liblzma5 libicu67 libzstd1 libxapian30 libcurl3-gnutls
 
 if [ $LMARCH == 'arm64' ]; then
   wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/libzim7_7.2.0.0_arm64.deb
