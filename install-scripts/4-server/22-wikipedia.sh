75942456561f64ae6cca5d4cdc426374db85d5b9
mgrouch <mgrushinskiy@gmail.com>
2022-09-06 10:24:01 -0400

    kiwix

--- install-scripts/4-server/22-wikipedia.sh
+++ install-scripts/4-server/22-wikipedia.sh
@@ -4,8 +4,8 @@ apt-get install -y liblzma5 libicu67 libzstd1 libxapian30 libcurl3-gnutls aria2
 
 if [ $LMARCH == 'arm64' ]; then
   wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/libzim7_7.2.0.0_arm64.deb
-  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/libkiwix10_0.0.0_arm64.deb
-  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/kiwix-tools_0.0.0_arm64.deb
+  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/libkiwix10_10.1.1.0_arm64.deb
+  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/kiwix-tools_3.3.0.0_arm64.deb
   dpkg -i libzim7_*.deb libkiwix10_*.deb kiwix-tools_*.deb
   rm libzim7_*.deb libkiwix10_*.deb kiwix-tools_*.deb
 fi
--- install-scripts/8-apps/19-wikipedia.sh
+++ install-scripts/8-apps/19-wikipedia.sh
@@ -3,7 +3,7 @@
 apt-get -y install libqt5webenginecore5 libqt5webenginewidgets5
 
 if [ $LMARCH == 'arm64' ]; then
-  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/kiwix_0.0.0_arm64.deb
+  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/kiwix_2.2.0.0_arm64.deb
   dpkg -i kiwix_*.deb
   rm kiwix_*.deb
 fi
