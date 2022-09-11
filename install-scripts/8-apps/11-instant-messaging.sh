d02df9c60f20c977625fcc614a132c283a801d2f
mgrouch <mgrushinskiy@gmail.com>
2022-09-11 07:40:48 -0400

    upd

--- install-scripts/8-apps/11-instant-messaging.sh
+++ install-scripts/8-apps/11-instant-messaging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt-get -y -q install empathy libayatana-appindicator3-1=0.5.5-2
+apt-get -y -q install empathy libayatana-appindicator3-1
 
 apt-get clean
 
@@ -15,11 +15,12 @@ else
   arch=$LMARCH
 fi
 
-wget http://ftp.us.debian.org/debian/pool/main/libi/libindicator/libindicator3-7_0.5.0-4_${arch}.deb
-wget http://ftp.us.debian.org/debian/pool/main/liba/libappindicator/libappindicator3-1_0.4.92-7_${arch}.deb
-dpkg -i libindicator3-7_0.5.0-4_${arch}.deb libappindicator3-1_0.4.92-7_${arch}.deb
-rm libindicator3-7_0.5.0-4_${arch}.deb libappindicator3-1_0.4.92-7_${arch}.deb
+#wget http://ftp.us.debian.org/debian/pool/main/libi/libindicator/libindicator3-7_0.5.0-4_${arch}.deb
+#wget http://ftp.us.debian.org/debian/pool/main/liba/libappindicator/libappindicator3-1_0.4.92-7_${arch}.deb
+#dpkg -i libindicator3-7_0.5.0-4_${arch}.deb libappindicator3-1_0.4.92-7_${arch}.deb
+#rm libindicator3-7_0.5.0-4_${arch}.deb libappindicator3-1_0.4.92-7_${arch}.deb
 
 wget https://github.com/mquevill/caprine/releases/download/v2.54.1-ARM/caprine_2.54.1_${arch}.deb
-dpkg -i caprine_2.54.1_${arch}.deb
+#dpkg -i caprine_2.54.1_${arch}.deb
+dpkg-deb -xv caprine_2.54.1_${arch}.deb /
 rm caprine_2.54.1_${arch}.deb
