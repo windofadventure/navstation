538e4a4de77b19eb05f85ff07bb77776fabaf2d1
mgrouch <mgrushinskiy@gmail.com>
2022-09-17 15:05:44 -0400

    upd

--- install-scripts/8-apps/19-wikipedia.sh
+++ install-scripts/8-apps/19-wikipedia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt-get -y install libqt5webenginecore5 libqt5webenginewidgets5
+apt-get -y install libqt5webenginecore5 libqt5webenginewidgets5 zim-tools
 
 if [ $LMARCH == 'arm64' ]; then
   wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/kiwix_2.2.0.0_arm64.deb
