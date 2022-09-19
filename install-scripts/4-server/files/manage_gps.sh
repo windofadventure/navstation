17a7cd094510ad1ea8bf1dcef72b86c8ed8a6ff5
mgrouch <mgrushinskiy@gmail.com>
2022-09-19 09:55:48 -0400

    improvements

--- install-scripts/4-server/files/manage_gps.sh
+++ install-scripts/4-server/files/manage_gps.sh
@@ -9,12 +9,13 @@
       systemctl stop lysgpsd@"$1".service
     fi
   else
-    logger "This USB device is known as GPS and will be connected to gpsd on port 2947 /dev/ttyLYS_gps_$1"
     if [[ $1 == "0" ]] ; then
+      logger "This USB device is known as GPS and will be connected to gpsd on port 2947 /dev/ttyLYS_gps_$1"
       systemctl restart gpsd
       export GPSD_OPTIONS="-p"
       gpsdctl add /dev/ttyLYS_gps_0
     else
+      logger "This USB device is known as GPS and will be connected to gpsd on port 2947$1 /dev/ttyLYS_gps_$1"
       systemctl restart lysgpsd@"$1".service
       sleep 1
       systemctl is-enabled signalk && systemctl restart signalk
