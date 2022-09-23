6eeb05c21e6a6ee6fc0ef76fcd85d424d77815b5
mgrouch <mgrushinskiy@gmail.com>
2022-09-22 22:49:47 -0400

    pypilot port fix

--- install-scripts/4-server/02-pypilot.sh
+++ install-scripts/4-server/02-pypilot.sh
@@ -160,5 +160,5 @@ sed '1 i :msg, contains, "No IMU Detected" stop' -i /etc/rsyslog.conf
 sed '1 i :msg, contains, "Failed to open I2C bus" stop' -i /etc/rsyslog.conf
 sed '1 i :msg, contains, "Using fusion algorithm Kalman" stop' -i /etc/rsyslog.conf
 
-# pypilot
+# prevent pypilot from changing port
 sed -i 's/8000/8080/' /etc/systemd/system/pypilot_web.service || true
