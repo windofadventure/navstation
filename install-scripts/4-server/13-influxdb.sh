136b9fab4ac147fc027fd2d65e733a0f9487f97c
mgrouch <mgrushinskiy@gmail.com>
2022-09-28 09:24:33 -0400

    upd

--- install-scripts/4-server/13-influxdb.sh
+++ install-scripts/4-server/13-influxdb.sh
@@ -2,10 +2,11 @@
 
 apt-get clean
 
-apt-get -y -q install influxdb chronograf kapacitor
+apt-get -y -q install influxdb chronograf kapacitor telegraf
 
 systemctl unmask influxdb
 systemctl disable influxdb
 
 systemctl disable chronograf
 systemctl disable kapacitor
+systemctl disable telegraf
