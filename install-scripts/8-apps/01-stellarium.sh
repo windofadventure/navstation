2a9234c57e3fdc50aa887653952525b665a071bc
mgrouch <mgrushinskiy@gmail.com>
2022-10-04 23:38:18 -0400

    fix

--- install-scripts/8-apps/01-stellarium.sh
+++ install-scripts/8-apps/01-stellarium.sh
@@ -7,7 +7,10 @@ apt-get -y -q install geographiclib-tools libqt5charts5 libqt5multimediawidgets5
 wget http://ppa.launchpad.net/stellarium/stellarium-releases/ubuntu/pool/main/s/stellarium/stellarium_1.0-upstream1.0~ubuntu20.04.1_arm64.deb
 wget http://ppa.launchpad.net/stellarium/stellarium-releases/ubuntu/pool/main/s/stellarium/stellarium-data_1.0-upstream1.0~ubuntu20.04.1_all.deb
 
-dpkg --ignore-depends=qtbase-abi-5-12-8 -i stellarium*.deb
+sudo dpkg-deb -xv stellarium_*.deb /
+sudo dpkg-deb -xv stellarium-data*.deb /
+sudo chown root:root /
+sudo chmod 755 /
 rm -f stellarium*.deb
 
 install -d -o 1000 -g 1000 -m 0755 "/home/user/.stellarium"
