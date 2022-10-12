af23667abced891e573616b1ed334c37adf43ec6
mgrouch <mgrushinskiy@gmail.com>
2022-10-12 09:06:16 -0400

    inspections fixes

--- install-scripts/2-baseSystem/03-ssh.sh
+++ install-scripts/2-baseSystem/03-ssh.sh
@@ -3,6 +3,6 @@
 apt-get install -y -q openssh-server
 
 ## If nothing specified (line commented), then forbid login as root
-sed 's/^\#PermitRootLogin\ */PermitRootLogin\ no\ \#/g' /etc/ssh/sshd_config > /etc/ssh/sshd_config
+sed -i 's/^\#PermitRootLogin\ */PermitRootLogin\ no\ \#/g' /etc/ssh/sshd_config 
 
 systemctl enable ssh
