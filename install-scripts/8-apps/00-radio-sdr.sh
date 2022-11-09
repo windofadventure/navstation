73a2d59f4fef4d48ebe177b60c07222ff50dc9bd
mgrouch <mgrushinskiy@gmail.com>
2022-11-09 14:01:27 -0500

    https://github.com/bareboat-necessities/lysmarine_gen/issues/193

--- install-scripts/8-apps/00-radio-sdr.sh
+++ install-scripts/8-apps/00-radio-sdr.sh
@@ -297,3 +297,15 @@ cd kalibrate-rtl/
 cp src/kal /usr/local/bin/
 cd ..
 rm -rf kalibrate-rtl/
+
+# AIS-Catcher https://github.com/jvde-github/AIS-catcher
+apt-get install -y librtlsdr0 libairspy0 libairspyhf1 \
+  libhackrf0 libsoapysdr0.7 libzmq3-dev libcurl4-openssl-dev zlib1g
+
+wget -q -O - https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/AIS-catcher-20221109-arm64.zip > AIS-catcher.zip
+unzip AIS-catcher.zip && rm AIS-catcher.zip
+mv AIS-catcher /usr/local/bin/ && chmod +x /usr/local/bin/AIS-catcher
+
+
+
+
