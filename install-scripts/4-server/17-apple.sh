838bde2b9bf4328dd5b2d81898fe5dc40357ed8e
mgrouch <mgrushinskiy@gmail.com>
2022-11-09 19:53:09 -0500

    https://github.com/bareboat-necessities/lysmarine_gen/issues/238

--- install-scripts/4-server/17-apple.sh
+++ install-scripts/4-server/17-apple.sh
@@ -10,4 +10,6 @@ systemctl enable shairport-sync
 
 install -m 755 -d -o usbmux -g plugdev "/var/lib/usbmux"
 
+install -v -m 0644 "$FILE_FOLDER"/52-carplay.rules "/etc/udev/rules.d/52-carplay.rules"
+
 apt-get clean
