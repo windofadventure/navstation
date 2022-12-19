2e75236d07fd5898ca39b2451926b4511272c28f
mgrouch <mgrushinskiy@gmail.com>
2022-12-19 14:03:37 -0500

    upd

--- install-scripts/4-server/23-tides.sh
+++ install-scripts/4-server/23-tides.sh
@@ -2,9 +2,12 @@
 
 # See: https://flaterco.com/xtide/files.html
 
+install -d /usr/share/opencpn
+install -d /usr/share/opencpn/tcdata
+
 wget https://flaterco.com/files/xtide/harmonics-dwf-20220109-free.tar.xz
 xzcat harmonics-dwf-20220109-free.tar.xz | tar xvf  -
 rm harmonics-dwf-20220109-free.tar.xz
-sudo cp -r harmonics-dwf-20220109 /usr/share/opencpn/tcdata/
+cp -r harmonics-dwf-20220109 /usr/share/opencpn/tcdata/
 rm -rf harmonics-dwf-20220109/
 
