33661621decac5ee1e0c1cf626fcc34e1dfbe604
mgrouch <mgrushinskiy@gmail.com>
2022-12-19 16:36:38 -0500

    upd

--- install-scripts/8-apps/files/stellarium-augmented.sh
+++ install-scripts/8-apps/files/stellarium-augmented.sh
@@ -2,7 +2,7 @@
 
 # hack to control Stellarium azimuth by true north heading from signalK
 
-MESA_GL_VERSION_OVERRIDE=3.0 MESA_GLSL_VERSION_OVERRIDE=130 onlyone stellarium "$@"  &
+MESA_GL_VERSION_OVERRIDE=2.1 MESA_GLSL_VERSION_OVERRIDE=130 onlyone stellarium "$@"  &
 sleep 12
 
 i=0
