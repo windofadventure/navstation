4c0b4725db28371f11bc94d42c80bca14400db11
mgrouch <mgrushinskiy@gmail.com>
2022-12-21 08:48:57 -0500

    fix

--- install-scripts/6-desktop/14-navtex.sh
+++ install-scripts/6-desktop/14-navtex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-apt-get install -y zlib1g libqt5sql5 libqt5serialport5 libqt5widgets5 libqt5dbus5 qtchooser pc-navtex
+apt-get install -y zlib1g libqt5sql5 libqt5serialport5 libqt5widgets5 libqt5dbus5 libqt5sql5-sqlite qtchooser pc-navtex
