91527d7310c7a4f99be12899ef1bf46ed37c298e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-02-26 00:33:49 +0000

    Update 00-addRepo.sh

--- install-scripts/0-repositories/00-addRepo.sh
+++ install-scripts/0-repositories/00-addRepo.sh
@@ -68,7 +68,7 @@ wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor > /usr/share/keyrin
 
 wget -q https://repos.influxdata.com/influxdata-archive_compat.key
 echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
-echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian bullseye main' | tee /etc/apt/sources.list.d/influxdata.list
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | tee /etc/apt/sources.list.d/influxdata.list
 rm influxdata-archive_compat.key
 
 ## Update && Upgrade
