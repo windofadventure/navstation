ffbb229f4f38af923e1d66402d7f4fff12bd7d66
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-05 05:14:14 +0000

    Update 17-drones.sh

--- install-scripts/6-desktop/17-drones.sh
+++ install-scripts/6-desktop/17-drones.sh
@@ -3,10 +3,10 @@
 # https://github.com/ArduPilot/apm_planner
 
 if [ "$LMARCH" == 'armhf' ]; then
-  wget https://github.com/bareboat-necessities/apm_planner_4rpi/releases/download/v2.0.29-rc1-16-g339533bfb/apmplanner2_2.0.29-rc1-16-g339533bfb_armhf.deb -O apmplanner2.deb
+  wget https://github.com/bareboat-necessities/apm_planner_4rpi/releases/download/v2.0.29-rc1-36-g1fba2b8fc/apmplanner2_2.0.29-rc1-36-g1fba2b8fc_armhf.deb -O apmplanner2.deb
 fi
 if [ "$LMARCH" == 'arm64' ]; then
-  wget https://github.com/bareboat-necessities/apm_planner_4rpi/releases/download/v2.0.29-rc1-16-g339533bfb/apmplanner2_2.0.29-rc1-16-g339533bfb_arm64.deb -O apmplanner2.deb
+  wget https://github.com/bareboat-necessities/apm_planner_4rpi/releases/download/v2.0.29-rc1-36-g1fba2b8fc/apmplanner2_2.0.29-rc1-36-g1fba2b8fc_arm64.deb -O apmplanner2.deb
 fi
 
 dpkg -i apmplanner2.deb && rm -f apmplanner2.deb
