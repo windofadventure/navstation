2d7e22cd04bbd415e375279360e8285200e820f4
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-24 10:38:06 -0400

    Update 18-solarthing.sh

--- install-scripts/8-apps/18-solarthing.sh
+++ install-scripts/8-apps/18-solarthing.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
-curl https://raw.githubusercontent.com/wildmountainfarms/solarthing/master/other/linux/clone_install.sh | bash
+useradd -r -g solarthing -G dialout,tty,video solarthing
+usermod -a -G dialout,tty,video solarthing
+usermod -a -G gpio solarthing
 usermod -a -G solarthing,dialout,tty,video user
+
+curl https://raw.githubusercontent.com/wildmountainfarms/solarthing/master/other/linux/clone_install.sh | bash
 /opt/solarthing/program/.bin/solarthing version || true
 rm -rf ./*.core ./*.log
 rm -rf /opt/solarthing/.git
