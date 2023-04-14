0fe1e2e058c01a757aa2a4119469569505fa52fb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-14 10:02:52 -0400

    Update 12-mosquitto.sh

--- install-scripts/4-server/12-mosquitto.sh
+++ install-scripts/4-server/12-mosquitto.sh
@@ -2,6 +2,9 @@
 
 apt-get -y -q install mosquitto mosquitto-clients
 
-install -d -o mosquitto -g mosquitto "/var/log/mosquitto/"
+install -d -o mosquitto -g mosquitto "/var/log/mosquitto
+
+# for mDNS
+install -v -m 0644 "$FILE_FOLDER"/mosquitto_service "/etc/avahi/services/mosquitto.service"
 
 systemctl disable mosquitto
