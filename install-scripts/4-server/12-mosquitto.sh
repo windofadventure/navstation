9cf4b3923b64ba4ba406258724da29fea9ae60ba
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-14 10:30:55 -0400

    Update 12-mosquitto.sh

--- install-scripts/4-server/12-mosquitto.sh
+++ install-scripts/4-server/12-mosquitto.sh
@@ -2,7 +2,7 @@
 
 apt-get -y -q install mosquitto mosquitto-clients
 
-install -d -o mosquitto -g mosquitto "/var/log/mosquitto
+install -d -o mosquitto -g mosquitto "/var/log/mosquitto"
 
 # for mDNS
 install -v -m 0644 "$FILE_FOLDER"/mosquitto_service "/etc/avahi/services/mosquitto.service"
